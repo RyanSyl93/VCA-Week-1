eeba99e83509caf75c49dce3326311a776589aca
Sylvester <CMPRSYLV@ljmu.ac.uk>
2021-10-01 10:08:57 +0100

    Set up

---
+++ test.js
@@ -0,0 +1,12 @@
+//first NodeJS
+
+applicationconstexpress=require('express')
+constapp=express()
+constport=3000
+
+app.get('/',(req,res) => {
+    res.send('Hello VirtualMachine!')})
+    
+app.listen(port,() => 
+    {console.log(`Express Application  listening at port 3000`)
+})
